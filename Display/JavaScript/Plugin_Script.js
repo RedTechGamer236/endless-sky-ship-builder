cce7d0de64fe3e8b665a82c6c58b880a4ba2dff4
GIVEMEFOOD <danielallcorn5@gmail.com>
2026-01-18 13:54:08 +0000

    Update Plugin_Script.js

--- Display/JavaScript/Plugin_Script.js
+++ Display/JavaScript/Plugin_Script.js
@@ -49,7 +49,7 @@ async function loadData() {
                     const rawText = await shipsResponse.text();
                     if (rawText.includes("version")) {
                         hardFindPluginFolder = await fetch(`${apiServerUrl}/repos/${repoUrl}/contents/data/${plugin.name}?ref=main`)
-                        extractHardFindPluginFolder = hardFindPluginFolder.json();
+                        extractHardFindPluginFolder = await hardFindPluginFolder.json();
 
                         extractHash = rawText.match(/oid\s+sha256:([a-f0-9]{64})/i);
                         hash = match[1];
